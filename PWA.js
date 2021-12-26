f4074976817d5c55334a7a20eb8c17180cf9f00c
The Polar'XS <96665300+thepolarxs@users.noreply.github.com>
2021-12-25 23:05:45 -0300

    Add files via upload

---
+++ PWA.js
@@ -0,0 +1,19 @@
+
+{
+	"version": "versão dels",
+	"download": "link do seu bagulho ( sempre atualiza) ",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": " raw do changelog en"
+		},
+		{
+			"languaje": "es",
+			"changelog": "raw do changelog es"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "raw do Changelog pt"
+		}
+	]
+}
