3d3a00e2c3c8dd31e0a9154ef313841b1a7d8942
The Polar'XS <96665300+thepolarxs@users.noreply.github.com>
2022-01-14 20:27:41 -0400

    Create PWAMINI.js

---
+++ PWAMINI.js
@@ -0,0 +1,18 @@
+{
+	"version": "1.0",
+	"download": "https://youtube.com/c/PolarModz%E3%82%81",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/thepolarxs/PWA/main/changelog-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/thepolarxs/PWA/main/changelog-es.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/thepolarxs/PWA/main/changelog-pt.html"
+		}
+	]
+}
